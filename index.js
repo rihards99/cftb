bb63cda26c060d825391d1ab4c74ea5ffebf27f3
Rihards Polis <rihards99@gmail.com>
2022-06-14 15:10:19 +0300

    Backend and DB setup
    * Setup for the api endpoints that provide data and
      receive post requests with changes
    * Setup sqlite to store data
    
    Getting all the comments and submitting a comment work, but
    upvoting is still missing.

--- index.js
+++ index.js
@@ -1,12 +1,92 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
+const bodyParser = require("body-parser");
+
 const app = express()
 const port = 3000
+const dbSource = "db.sqlite"
+
+let db = new sqlite3.Database(dbSource, (err) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message)
+    throw err
+  } else {
+      console.log('Connected to the SQLite database.')
+      db.run(`
+          CREATE TABLE comments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp text,
+            commentText text,
+            name text,
+            thumbnail text
+          )
+        `,
+      (err) => {
+          if (err) {
+              // Table already created
+          } else {
+              // Table just created, creating some rows
+              const insert = 'INSERT INTO comments (timestamp, commentText, name, thumbnail) VALUES (?,?,?,?)'
+              db.run(insert, [
+                '1655190034000',
+                `Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.`,
+                'Rob Hope',
+                'https://randomuser.me/api/portraits/thumb/men/3.jpg',
+              ])
+              db.run(insert, [
+                '1655203709486',
+                `Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)`,
+                'Sophie Brecht',
+                'https://randomuser.me/api/portraits/thumb/women/3.jpg',
+              ])
+          }
+      });
+  }
+});
 
 app.use(express.static('static'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/comments', (req, res) => {
+  db.all('SELECT * FROM comments ORDER BY timestamp DESC', [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      comments: rows.map(row => ({
+        id: row.id,
+        timestamp: row.timestamp,
+        user: {
+          name: row.name,
+          thumbnail: row.thumbnail,
+        },
+        text: row.commentText,
+
+      }))
+    })
+  });
+})
+
+app.post('/comment', (req, res) => {
+  const data = req.body;
+  const insert = 'INSERT INTO comments (timestamp, commentText, name, thumbnail) VALUES (?,?,?,?)'
+  db.run(insert, [
+    data.timestamp,
+    data.commentText,
+    data.name,
+    data.thumbnail,
+  ], (err) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.json({ status: 'SUCCESS'})
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- static/index.js
+++ static/index.js
@@ -1,5 +1,37 @@
-const onCommentSubmit = () => {
-  console.log("asd")
+window.commentData = null
+
+const onCommentSubmit = async () => {
+  const inputElement = document.querySelector('.commentInput')
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  const data = {
+    name: `${userData.name.first} ${userData.name.last}`,
+    thumbnail: userData.picture.thumbnail,
+    timestamp: new Date().getTime() + "",
+    commentText: inputElement.value,
+  };
+
+  try {
+    const response = await fetch('/comment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const json = await response.json();
+
+    if (json?.status === 'SUCCESS') {
+      loadComments();
+      inputElement.value = "";
+    } else {
+      throw new Error()
+    }
+  } catch (e) {
+    alert("Can't submit comment");
+  }
+
 }
 
 const generateUserData = async () => {
@@ -18,10 +50,95 @@ const generateUserData = async () => {
   }
 }
 
-window.onload = async () => {
-  const userData = localStorage.getItem("userData");
-  if (!userData) {
-    const userData = await generateUserData();
-    localStorage.setItem("userData", userData)
+const getComments = async () => {
+  try {
+    const res = await fetch("/comments");
+    const data = await res.json();
+    if (data && data.comments) {
+      return data.comments;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    alert("Can't retrieve comment data");
+    return {};
   }
+}
+
+// From https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
+var getRelativeTime = (d1, d2 = new Date()) => {
+  const units = {
+    year  : 24 * 60 * 60 * 1000 * 365,
+    month : 24 * 60 * 60 * 1000 * 365/12,
+    day   : 24 * 60 * 60 * 1000,
+    hour  : 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000
+  }
+
+  var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+  var elapsed = d1 - d2
+
+  // "Math.abs" accounts for both "past" & "future" scenarios
+  for (var u in units)
+    if (Math.abs(elapsed) > units[u] || u == 'second') 
+      return rtf.format(Math.round(elapsed/units[u]), u)
+}
+
+const commentTemplate = (imageUrl, userName, timestamp, text) => `
+  <div class="comment">
+    <img class="thumbnail" src="${imageUrl}" alt="Your image">
+    <div class="commentTextWrapper">
+      <span class="commentName">${userName} <span class="commentTimestamp">・ ${getRelativeTime(+new Date(parseInt(timestamp)))}</span></span>
+      <div class="commentText">${text}</div>
+      <div class="commentButtons">
+        <button class="commentButton">▲ Upvote</button>
+        <button class="commentButton">Reply</button>
+      </div>
+    </div>
+  </div>
+`
+
+const renderComments = () => {
+  if (window.commentData === null) {
+    return // TODO: Could add a spinner render instead of starting
+  }
+
+  if (window.commentData.length === 0) {
+    return // TODO: Render some "No comments yet stuff"
+  }
+
+  const commentsElement = document.querySelector('.comments')
+  commentsElement.innerHTML = window.commentData
+    .map(comment => 
+      commentTemplate(
+        comment.user.thumbnail,
+        comment.user.name,
+        comment.timestamp,
+        comment.text
+      )
+    )
+    .join("")
+
+}
+
+const loadComments = async () => {
+  window.commentData = await getComments()
+  renderComments()
+}
+
+const renderUserLine = (thumbnail) => {
+  const thumbnailElement = document.querySelector('.userLine .thumbnail')
+  thumbnailElement.setAttribute('src', thumbnail)
+}
+
+window.onload = async () => {
+  let userData = JSON.parse(localStorage.getItem("userData"));
+  if (!userData) {
+    userData = await generateUserData();
+    localStorage.setItem("userData", JSON.stringify(userData))
+  }
+
+  renderUserLine(userData.picture.thumbnail)
+  loadComments()
 }
